9244735f2424700030470ae8c9af9b70ced57e5c
Niklas Rosenstein <rosensteinniklas@gmail.com>
2020-04-26 22:45:21 +0200

    update default

--- src/nr/powerline/main.py
+++ src/nr/powerline/main.py
@@ -12,11 +12,12 @@ def default_powerline():
   from nr.powerline import PowerLine
   p = PowerLine()
   p.set_pen('white', 'magenta')
-  p.add_part(' {c.DIRECTORY} {session.cwd} !{c.RIGHT_TRIANGLE} ')
+  p.add_part(' {c.DIRECTORY} {session.cwd} !{c.RIGHT_TRIANGLE}')
   git = p.get_plugin('git')
   if git.project:
     p.set_pen(None, 'blue')
     p.add_part(' {c.BRANCH} {git.branch} !{c.RIGHT_TRIANGLE}')
+  p.add_part(' ')
   p.clear_pen()
   return p
 
