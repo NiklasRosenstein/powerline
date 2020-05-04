7d924fc4066b8603b36bd75d089e34956538a0d9
Niklas Rosenstein <rosensteinniklas@gmail.com>
2020-05-04 23:54:08 +0200

    update TextPlugin

--- src/nr/powerline/text.py
+++ src/nr/powerline/text.py
@@ -29,19 +29,34 @@ from typing import Iterable, List
 
 @implements(PowerlinePlugin)
 class TextPlugin(Struct):
+  """
+  Displays some text in the specified *style*. Some additional flags can change the
+  style based on some condition. Available conditions are
+
+  * is-server-indicator: Swaps to *indicator_style* if the powerline is rendered
+    from a server. Swaps to *indicator_style_2* if no environment data is passed
+    to the server.
+  * is-status-indicator: Swaps to *indicator_style* if the status code is non-zero.
+  """
+
   style = Field(ansi.Style, default=None)
   is_server_indicator = Field(bool, FieldName('is-server-indicator'), default=False)
   is_status_indicator = Field(bool, FieldName('is-status-indicator'), default=False)
   indicator_style = Field(ansi.Style, FieldName('indicator-style'), default=ansi.parse_style('bg:red bold'))
+  indicator_style_2 = Field(ansi.Style, FieldName('indicator-style-2'), default=ansi.parse_style('fg:yellow'))
   text = Field(str)
 
   @override
   def render(self, context: PowerlineContext) -> Iterable[Pen]:
     if self.is_server_indicator and context.is_server:
-      style = self.indicator_style
+      if not context.env:
+        style = self.indicator_style_2
+      else:
+        style = self.indicator_style
     elif self.is_status_indicator and context.exit_code != 0:
       style = self.indicator_style
     else:
       style = self.style or context.default_style
+    style = style.merge(self.style or context.default_style)
     yield Pen.Text(self.text, style)
     yield Pen.Flipchar(chars.RIGHT_TRIANGLE)
